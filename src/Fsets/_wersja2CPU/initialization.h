eb8d75cde9fdb6cef4ab0a53046405ccbbff8fb9
Tomasz Kujawa <tomasz.kujawa@gmail.com>
2010-03-06 21:00:11 +0100

    Master Thesis and its source from Ph.D. W. Andrzejewski

---
+++ src/Fsets/_wersja2CPU/initialization.h
@@ -0,0 +1,15 @@
+#ifndef INITIALIZATION_H
+#define INITIALIZATION_H
+
+
+#include <stdio.h>
+#include <math.h>
+#include "constants.h"
+#include "utils.h"
+
+void generateData(int* tab);
+void generateData2(int* tab);
+void generateData3(int* tab);
+
+#endif
+
