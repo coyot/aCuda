a154922e2b7c140b9039cf6ac72bae49e084355a
Tomasz Kujawa <tomasz.kujawa@gmail.com>
2010-05-29 16:15:51 +0200

    finished project preparation

---
+++ src/aCuda/aCudaResearch/aCudaResearch/ACudaMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace aCudaResearch
+{
+    class ACudaMain
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This is my master thesis research app.");
+            Console.ReadKey();
+        }
+    }
+}
