70f6b15af38bc28b034255e217ad5708d552a92b
Tomasz Kujawa <tomasz.kujawa@gmail.com>
2010-06-01 22:04:13 +0200

    code contract on interface - simple implementation; also added to ignore file line to ignore xxx.dll files

--- src/aCuda/aCudaResearch/aCudaResearch.Tests/aCudaResearch.Designer.cs
+++ src/aCuda/aCudaResearch/aCudaResearch.Tests/aCudaResearch.Designer.cs
@@ -1,2 +1,2 @@
-//64cc1d37
-//1t1rTzfUh+hma7MH2RK8hWyoC3c=9HRxYZOkH+NCf+Gd/H0HvjM92ec=
+//a02318c7
+//Cc7pM3TWJmcIE6xGqg4RLAyoyNI=9HRxYZOkH+NCf+Gd/H0HvjM92ec=
--- src/aCuda/aCudaResearch/aCudaResearch/Settings/ISettingsBuilder.cs
+++ src/aCuda/aCudaResearch/aCudaResearch/Settings/ISettingsBuilder.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics.Contracts;
 
 namespace aCudaResearch.Settings
 {
+    /// <summary>
+    /// Definition of builder interface to prepare <see link="ExecutionSettings"/> ojbect.
+    /// </summary>
+    [ContractClass(typeof(ISettingsBuilderContract))]
     interface ISettingsBuilder
     {
         /// <summary>
@@ -12,5 +17,22 @@ namespace aCudaResearch.Settings
         /// </summary>
         /// <returns><see link="ExecutionSettings"/></returns>
         ExecutionSettings Build();
+    }
+
+    [ContractClassFor(typeof(ISettingsBuilder))]
+    abstract class ISettingsBuilderContract : ISettingsBuilder
+    {
+        #region ISettingsBuilder Members
+
+        public ExecutionSettings Build()
+        {
+            Contract.Ensures(Contract.Result<ExecutionSettings>() != null);
+
+            // This is not important because the CodeContract engine will not allowe to execute it
+            // but on the other hand there MUST be returning value.
+            return default(ExecutionSettings);
+        }
+
+        #endregion
     }
 }
