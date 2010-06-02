a628d24d257af6e9991ed45097b946f816624e37
Tomasz Kujawa <tomasz.kujawa@gmail.com>
2010-06-02 22:53:19 +0200

    revised the structure of the project

---
+++ src/aCuda/aCudaResearch/aCudaResearch/FpGrowth/Data/MsDataBuilder.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace aCudaResearch.FpGrowth.Data
+{
+    public class MsDataBuilder : IDataBuilder<MsInstance<int>>
+    {
+
+    }
+}
---
+++ src/aCuda/aCudaResearch/aCudaResearch/FpGrowth/Data/MsWeb/Element.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace aCudaResearch.FpGrowth.Data.MsWeb
+{
+    /// <summary>
+    /// Describes entry from the MsWeb data.
+    /// </summary>
+    class Element
+    {
+        /// <summary>
+        /// Page title.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Page URL address.
+        /// </summary>
+        public string Url { get; set; }
+    }
+}
