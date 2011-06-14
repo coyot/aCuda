4fc0b16ae3820ea0aeac1a0cdfe9696019fd11b1
Tomasz Kujawa <tomasz.kujawa@gmail.com>
2011-06-14 10:15:31 +0200

    some constants and the helper which uses CUDA computation engine

---
+++ src/aCuda/aCudaResearch/aCudaResearch/Cuda/Names.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace aCudaResearch.Cuda
+{
+    public class Names
+    {
+        public static string CountFrequency = "count_frequency";
+        public static string CountSetsFrequencyMatrix = "count_frequency_matrix";
+        public static string CountSetsFrequencyTable = "count_frequency_table";
+    }
+}
